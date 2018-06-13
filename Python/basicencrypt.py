83cedb7a0f75a3ad86dc1f6511b90729d91fd773
Nicholas <blockpit@gmail.com>
2018-06-13 20:16:18 +1000

    Add files via upload

---
+++ Python/basicencrypt.py
@@ -0,0 +1,10 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+plaintext = 'helloworld'
+
+ciphertext = 'helloworld'
+
+for i in range( len( plaintext ) ):
+    ciphertext[i+2] = plaintext[i]
+
+print(ciphertext)
