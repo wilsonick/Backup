86b51537601a0ee882dc44a0b1afbf348b2a463a
Nicholas <blockpit@gmail.com>
2018-06-14 20:18:54 +1000

    Add files via upload

---
+++ Python/Exp/ninth.py
@@ -0,0 +1,29 @@
+# Standard library experimentation
+
+print(abs(-2))
+
+print( all( [True,True,True,True,True] ) )
+
+print( all( [True,True,True,False] ) )
+
+print( any( [False,False,False,False] ) )
+
+print( any( [False,False,False,True] ) )
+
+print( ascii('nhuoehoeĤģĴ') )
+
+print( bin(93827) )
+
+print( bin(100) )
+
+print( bin(-7932) )
+
+print( bool() )
+
+print( bool(420) )
+
+print( bool(3328904032) )
+
+print( bytearray (9) )
+
+print( 
